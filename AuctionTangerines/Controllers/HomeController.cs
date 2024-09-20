0c9a69ee3b94536fa40942c0da811728a9dd255d
KomaMix <ksabirov448@gmail.com>
2024-09-20 20:06:28 +0300

    Создана начальная авторизация и вход в систему

--- AuctionTangerines/Controllers/HomeController.cs
+++ AuctionTangerines/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using AuctionTangerines.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace AuctionTangerines.Controllers
 {
+	[Authorize]
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
---
+++ AuctionTangerines/DTOs/RegisterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AuctionTangerines.DTOs
+{
+	public class RegisterDto
+	{
+		public string Email { get; set; }
+		public string Password { get; set; }
+	}
+}
