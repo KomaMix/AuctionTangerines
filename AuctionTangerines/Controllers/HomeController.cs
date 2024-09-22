630e91222cc7ad586395a672294263c22131ca0f
KomaMix <ksabirov448@gmail.com>
2024-09-22 20:03:18 +0300

    Добавил отображение списка мандарин

--- AuctionTangerines/Controllers/HomeController.cs
+++ AuctionTangerines/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
+using AuctionTangerines.Data;
 using AuctionTangerines.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace AuctionTangerines.Controllers
@@ -9,16 +11,22 @@ namespace AuctionTangerines.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _dbContext;
 
-		public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext dbContext)
 		{
+			_dbContext = dbContext;
 			_logger = logger;
 		}
 
-		public IActionResult Index()
+		public async Task<IActionResult> Index()
 		{
-			return View();
-		}
+            // Получаем список мандаринок из базы данных
+            var tangerines = await _dbContext.Tangerines.ToListAsync();
+
+            // Передаем список в представление
+            return View(tangerines);
+        }
 
 		public IActionResult Privacy()
 		{
